9b732773cc6b16811f65509d50988db398e5bb1c
Ahmed Gamal <sci.ahmedgamal40520@alexu.edu.eg>
2021-08-21 22:53:19 +0200

    Minor client side changes, Styling, README.md.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Weather App by ZipCode
+A simple Web app that takes a ZIP code and your current mood as input, and returns current temp and your mood :)
+
+# Preview
+![Demo](/demo.gif)
+
+# How to Use
+
+## Must install
+* [Node.js](https://nodejs.org/en/download/)
+* Express (by running `npm install express` in the Terminal)
+* Body Parser (by running `npm install body-parser` in the Terminal)
+
+# Functionality
+## Navigation
+There is a Navbar containing quick-scroll to desiered section
+
+## Storing and retreiving data
+using `Node`, `Express` and `Body Parser` I can get data from the user, store it and use it to get data from the [Open Weather API](https://openweathermap.org/).
+Then i display the data received from the `API` along with the user's mood.
+
+# :)
+##### everything from design to how the code was writen is completly done by me from scratch.
--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = "2ce0b9ed74673bc52166c2445931ed4f";
+const apiKey = "YOUR API KEY HERE";
 const beforeApi = ",&appid=";
 // to get the temperature in Celsius
 const afterApi = "&units=metric";
@@ -12,76 +12,34 @@ let d = new Date();
 // we can replace the ubove code with this simple line
 let newDate = d.toDateString();
 
-// Main function
-const Weather = () => { 
-    // getting the values entered by the user for the zip and feelings
-    const zip = document.getElementById("zip").value;
-    const feelings = document.getElementById("feelings").value;
-  
-    // getWeather return promise
-    getWeather(zip).then((data) => {
-      //checking the data received and taking whats needed through destructuring the "data" object recieved
-      if (data) {
-        const {
-          main: {temp},
-          name: city,
-        } = data;
-  
-        const allInfoToDisplay = {
-          newDate,
-          city,
-          temp: Math.round(temp), // making it an integer
-          feelings,
-        };
-  
-        postData(server + "/add", allInfoToDisplay);
-  
-        updateUI();
-        document.getElementById('entryHolder').style.opacity = 1;
-      }
-    });
-};
-
 // getting the data from the weather api
 const getWeather = async (zip) => {
-    try {
-      const res = await fetch(baseURL + zip + beforeApi + apiKey + afterApi);
-      const data = await res.json();
-  
-    //   if (data.cod != 200) {
-    //     // display the error message on UI
-    //     error.innerHTML = data.message;
-    //     setTimeout(_=> error.innerHTML = '', 2000)
-    //     throw `${data.message}`;
-    //   }
-  
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const res = await fetch(baseURL + zip + beforeApi + apiKey + afterApi);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-// to check if the button is clicked
-document.getElementById("generate").addEventListener("click", Weather);
 
 // posting the data to server
 const postData = async (url = "", allInfoToDisplay = {}) => {
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(allInfoToDisplay),
-    });
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(allInfoToDisplay),
+  });
   
-    try {
-      const newData = await res.json();
-      console.log(`You just saved`, newData);
-      return newData;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // retreiving weather data from the server and updating the UI
 const updateUI = async () => {
@@ -92,8 +50,41 @@ const updateUI = async () => {
       document.getElementById("date").innerHTML = savedData.newDate;
       document.getElementById("city").innerHTML = savedData.city;
       document.getElementById("temp").innerHTML = savedData.temp + '&degC';
-      document.getElementById("content").innerHTML = savedData.feelings;
+      document.getElementById("content").innerHTML = savedData.mood;
     } catch (error) {
       console.log(error);
     }
   };
+
+
+  // Main function
+const Weather = () => { 
+  // getting the values entered by the user for the zip and mood
+  const zip = document.getElementById("zip").value;
+  const mood = document.getElementById("feelings").value;
+  // getWeather returns a promise, so we can use .then()
+  getWeather(zip).then((data) => {
+    //checking the data received and taking whats needed through destructuring the "data" object recieved
+    if (data) {
+      const {
+        main: {temp},
+        name: city,
+      } = data;
+  
+      const allInfoToDisplay = {
+        newDate,
+        city,
+        temp: Math.round(temp), // making it an integer
+        mood,
+      };
+  
+      postData(server + "/add", allInfoToDisplay);
+        
+      // calling to update the UI with collected INFO
+      updateUI();
+    }
+  });
+};
+
+// check if the button is clicked
+document.getElementById("generate").addEventListener("click", Weather);
